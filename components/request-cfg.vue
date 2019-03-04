2409dd565e51298c7ae71304e4b21741deab3e77
sculpt0r <calemb@gmail.com>
2019-03-04 07:23:19 +0100

    Switch dev port, extract axios as requester

---
+++ components/request-cfg.vue
@@ -0,0 +1,14 @@
+<script>
+import Axios from "axios";
+
+export default {
+  requester: Axios,
+  get: function(path) {
+    return this.requester.get(this.urlGame(path), { withCredentials: true });
+  },
+  urlGame: function(path) {
+    console.log("tst");
+    return "http://127.0.0.1:3000/game/" + path;
+  }
+};
+</script>
