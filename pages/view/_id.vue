f990b0013ff25efb9b173d64e9891833ff15aae6
unknown <calemb@gmail.com>
2019-04-23 21:51:31 +0200

    Add view user with id

---
+++ pages/view/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    ID: {{ id }}
+    MSG me!
+    FIGHT me!
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: ""
+    };
+  },
+  mounted: function() {
+    //WORKING  -get player with that id data
+    this.id = this.$nuxt._route.params;
+  }
+};
+</script>
+
+<style>
+</style>
