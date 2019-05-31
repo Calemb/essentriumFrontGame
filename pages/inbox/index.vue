cf011c901af87080a98f0a2977000b47b999a3ff
sculpt0r <calemb@gmail.com>
2019-05-31 05:38:26 +0200

    Msg inbox

--- pages/inbox/index.vue
+++ pages/inbox/index.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
     <h1> INBOX </h1>
+    <div
+      v-for="msg in msgs"
+      :key="msg._id"
+    >
+      <nuxt-link :to="'view/' + msg.adresser">Nadawca</nuxt-link>
+      :: {{ msg.title }}
+      <br>
+      {{ msg.content}}
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Requester from "~/components/request-cfg";
+export default {
+  data() {
+    return {
+      msgs: []
+    };
+  },
+  mounted: function() {
+    this.loadData();
+  },
+  methods: {
+    loadData: function() {
+      Requester.get("msg/msgs").then(result => {
+        this.msgs = result.data;
+      });
+    }
+  }
+};
 </script>
 
 <style>
