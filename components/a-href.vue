1af76f25984aa5b78b5a3c4db75530e521c1bcd2
sculpt0r <calemb@gmail.com>
2019-07-23 05:33:10 +0200

    Add inner link component

---
+++ components/a-href.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- @click="click($event)" -->
+  <nuxt-link v-bind:to="'/'+to">
+    {{ title }}
+  </nuxt-link>
+</template>
+
+<script>
+//pseudo static
+import requester from "~/components/request-cfg";
+
+export default {
+  props: {
+    title: String,
+    to: String
+  },
+  methods: {
+    defaultParse: function(response) {
+      console.log("res", response);
+      document.getElementById("nuxtApp").innerHTML = JSON.stringify(response);
+    },
+    click: function(event) {
+      console.log("a-href clicked");
+      if (event) {
+        event.preventDefault();
+        requester
+          .getPage(this.href)
+          .then(response => this.defaultParse(response.data))
+          .catch(response => this.defaultParse(response));
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
